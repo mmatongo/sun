36557972159849f1c87bddee76db274e9d112933
Daniel M. Matongo <mmatongo_@hotmail.com>
2024-01-14 20:06:33 +0000

    switch to unix time | add some basic err handling | add F -> C conversion

--- main.go
+++ main.go
@@ -1,31 +1,54 @@
 package main
 
 import (
+	"encoding/json"
+	fl "flag"
+	"fmt"
 	f "fmt"
 	"io"
-	net "net/http"
+	"net/http"
+	"os"
+	t "time"
 )
 
 type Weather struct {
 	ResolvedAddress string `json:"resolvedAddress"`
 	TimeZone        string `json:"timezone"`
 	Days            []struct {
-		DateTime     string `json:"datetime"`
-		Conditions   string `json:"conditions"`
-		Descriptions string `json:"descriptions"`
-		Hours        []struct {
-			DateTime   string `json:"datetime"`
-			Conditions string `json:"conditions"`
+		DatetimeEpoch int64  `json:"datetimeEpoch"`
+		Conditions    string `json:"conditions"`
+		Descriptions  string `json:"descriptions"`
+		Hours         []struct {
+			DatetimeEpoch int64 `json:"datetimeEpoch"`
+
+			Conditions string  `json:"conditions"`
+			Temp       float64 `json:"temp"`
 		} `json:"hours"`
 	} `json:"days"`
 	CurrentConditions struct {
-		DateTime   string `json:"datetime"`
-		Conditions string `json:"conditions"`
+		DatetimeEpoch int64   `json:"datetimeEpoch"`
+		Conditions    string  `json:"conditions"`
+		Temp          float64 `json:"temp"`
 	} `json:"currentConditions"`
 }
 
 func main() {
-	res, err := net.Get("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/bath?unitGroup=us&key=&contentType=json")
+	q := "bath"
+	var key string
+
+	if len(os.Args) >= 3 {
+		q = os.Args[1]
+	}
+	fl.StringVar(&key, "key", "NULL", "API KEY")
+	fl.Parse()
+
+	if key == "" {
+		fmt.Println("No API key provided. Exiting.")
+		os.Exit(1)
+	}
+
+	url := f.Sprintf("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/%s?unitGroup=us&key=%s&contentType=json", q, key)
+	res, err := http.Get(url)
 	if err != nil {
 		panic(err)
 	}
@@ -33,7 +56,7 @@ func main() {
 	defer res.Body.Close()
 
 	if res.StatusCode != 200 {
-		panic("Weather API not available")
+		panic("Weather API not available or API Key was not supplied")
 	}
 
 	body, err := io.ReadAll(res.Body)
@@ -41,5 +64,35 @@ func main() {
 		panic(err)
 	}
 
-	f.Println(string(body))
+	var weather Weather
+	err = json.Unmarshal(body, &weather)
+	if err != nil {
+		panic(err)
+	}
+	location, timezone, currentCondition, hours := weather.ResolvedAddress, weather.TimeZone, weather.CurrentConditions, weather.Days[0].Hours
+	//(°F − 32) × 5/9 = °C
+	temp := (currentCondition.Temp - 32.0) * 5 / 9
+
+	f.Printf("%s, %s: %.0f°C, %s\n",
+		location,
+		timezone,
+		temp,
+		currentCondition.Conditions,
+	)
+	for _, hour := range hours {
+		date := t.Unix(hour.DatetimeEpoch, 0)
+		// (°F − 32) × 5/9 = °C
+		temp := (hour.Temp - 32.0) * 5 / 9
+
+		if date.Before(t.Now()) {
+			continue
+		}
+
+		f.Printf("%s - %.0f°C, %s\n",
+			date.Format("15:04"),
+			temp,
+			hour.Conditions,
+		)
+	}
+
 }
